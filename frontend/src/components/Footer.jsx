a5c7effb065e5a6e186cb48359450789fe6219bc
Sayak-364 <168944774+Sayak-364@users.noreply.github.com>
2024-07-31 21:44:52 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,4 @@
 import React,{useState} from "react";
-import { BiBorderAll } from "react-icons/bi";
 import TwitterIcon from '@mui/icons-material/Twitter';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
