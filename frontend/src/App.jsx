e523254d9b7526fd5a1c096773c47d662e9f1a70
Sayak-364 <168944774+Sayak-364@users.noreply.github.com>
2024-07-31 20:51:01 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,8 @@ import Header from "./pages/Header"
 import Home from "./pages/Home"
 // import CarCard from "./components/CarCard"
 import CarSlider from "./components/CarSlider"
-
+import Footer from "./components/Footer"
+import Counter from "./components/Counter"
 export default function App() {
   return (
     <>
